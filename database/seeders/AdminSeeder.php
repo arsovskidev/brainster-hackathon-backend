8841e44ba8c9bab8a88a72d7e87242f7bfacfc1e
Blaga Angelovska <blaga.bojchevska@gmail.com>
2021-08-14 12:34:52 +0200

    breeze and adminseeder added

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user -> name = "Admin"; 
+        $user -> password = Hash::make('admin12345'); 
+        $user -> email = "admin@test.com"; 
+        $user -> save(); 
+    }
+}
