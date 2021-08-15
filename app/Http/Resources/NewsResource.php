9aa5f175cb610071ec30b3f7e10edce684612191
Филип Арсовски <arsovskidev@gmail.com>
2021-08-15 02:57:31 +0200

    Fixing news resources.

--- app/Http/Resources/NewsResource.php
+++ app/Http/Resources/NewsResource.php
@@ -17,12 +17,9 @@ class NewsResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'description' => $this->description,
             'content' => $this->content,
-            'image_first' => $this->image_first,
-            'image_second' => $this->image_second,
-            'image_third' => $this->image_third,
-            'image_fourth' => $this->image_fourth,
+            'image' => $this->image,
+            'date' => $this->date,
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
         ];
